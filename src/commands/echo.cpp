b76a59f78dabc6c8f5c07b9944ab1b7f1d3f20bc
GabJuarez <gajuarez@uamv.edu.ni>
2026-01-27 19:22:46 -0600

    Fixing echo

--- src/commands/echo.cpp
+++ src/commands/echo.cpp
@@ -4,12 +4,16 @@
 
 namespace sh::builtins {
     void echo(const std::vector<std::string> &args) {
+        bool first = true;
         for (const auto &arg: args) {
             if (arg == "<newline>") {
                 std::cout << std::endl;
+                first = true;
                 continue;
             }
-            std::cout << arg << " ";
+            if (!first) std::cout << ' ';
+            std::cout << arg;
+            first = false;
         }
         std::cout << std::endl;
     }
